38ed8fdda328a79af47a04e7639046af9b10f72d
Sindu Andita Pratama <sindu.andita@gmail.com>
2023-11-27 11:45:46 +0700

    fix test unit

--- src/components/BookingForm/BookingForm.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/rea';
-import BookForm from './BookingForm.component';
-
-test('Renders the BookingForm heading', () => {
-  render(<BookForm />);
-  const headingElement = screen.getByText('Reserve a Table');
-  expect(headingElement).toBeInTheDocument();
-});
